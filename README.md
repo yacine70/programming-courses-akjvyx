0adef1c0411796487fda92093654c5cbd806a07e
yacine70 <yacinebousbaci70@gmail.com>
2021-10-05 02:38:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-akjvyx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-akjvyx)
---
+++ index.ts
@@ -0,0 +1,52 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+console.log('bonjour tout le monde');
+console.log('je suis yacine');
+let num1: number;
+let num2: number;
+num1 = 123;
+num2 = 4527;
+
+console.log(num1);
+console.log(num2);
+console.log(num1 + num2);
+let addition: number;
+addition = num1 + num2;
+console.log('addition');
+console.log(addition);
+let num3: number;
+let num4: number;
+num3 = 2;
+num4 = 3;
+console.log(num3);
+console.log(num4);
+let soustraction: number;
+soustraction = num3 - num4;
+console.log('num3 - num4');
+console.log(soustraction);
+
+let mama: string;
+let papa: string;
+mama = "ma maman s'appelle zakia";
+papa = "mon papa s'appelle mohamed";
+console.log('mama');
+console.log(mama);
+console.log('papa');
+console.log(papa);
+let num5: number;
+num5 = 5;
+num5 = num5 + 10;
+console.log('num5');
+console.log(num5);
+let num6: number;
+let num7: number;
+let jus: number;
+num6 = 10;
+num7 = 20;
+jus = num6;
+num6 = num7;
+num7 = jus;
+console.log('num6=', num6);
+console.log('num7', num7);
